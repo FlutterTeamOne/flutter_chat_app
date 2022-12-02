a44d05cb7213fc46744629e01ddba2f07cef9aea
bukhanovskayamarina22 <116250199+bukhanovskayamarina22@users.noreply.github.com>
2022-12-02 22:47:23 +0300

    fixed length of the date strings

--- db/main.sqlite.sql
+++ db/main.sqlite.sql
@@ -13,10 +13,10 @@
 	friends_chat_id  integer NOT NULL,
 	sender_id        integer NOT NULL,
 	content          char(50) NOT NULL,
-	date            char(14) NOT NULL,
+	date            char(26) NOT NULL,
 	CONSTRAINT MESSAGES_FK_79 FOREIGN KEY ( friends_chat_id ) REFERENCES friends_chat ( main_friends_chat_id ),
 	CONSTRAINT MESSAGES_FK_80 FOREIGN KEY ( sender_id ) REFERENCES users ( main_users_id ),
-	CHECK (LENGTH(date) = 14)
+	CHECK (LENGTH(date) = 26)
 	);
 
 	CREATE TABLE users
@@ -24,9 +24,9 @@
 	main_users_id     integer PRIMARY KEY AUTOINCREMENT,
 	name              char(50) NOT NULL,
 	email             char(50) NOT NULL,
-	registration_date char(14) NOT NULL,
+	registration_date char(26) NOT NULL,
 	profile_pic_url   char(50) NOT NULL,
-	CHECK (LENGTH(registraion_date) = 14)
+	CHECK (LENGTH(registraion_date) = 26)
 	);
 	
 	-- INDEX
