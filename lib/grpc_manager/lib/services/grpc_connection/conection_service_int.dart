d0d51b3b5e02dd72e486ec4470a6146f762883f6
SharovIvanY <SharovIvanY@gmail.com>
2022-12-09 12:54:50 +0300

    delete

--- lib/grpc_manager/lib/services/grpc_connection/conection_service_int.dart
+++
@@ -1,9 +0,0 @@
-import 'package:grpc_connection/proto/generated/grpc_connection/grpc_connection.pb.dart';
-import 'package:grpc_connection/services/grpc_connection/connection_service.impl.dart';
-
-abstract class IConnectionServices {
-  factory IConnectionServices() => ConnectionServices();
-  Message? checkConnection(Message message);
-}
-
-final connectionServices = ConnectionServices();
