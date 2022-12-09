7f949fda174bce42788b8bdbe0619d1746fdf673
Slavlen <66310017+slavlen@users.noreply.github.com>
2022-12-09 17:00:26 +0300

    добавили старый тест от Артуда для последнего непрочитаного

--- test/modules/storage_manager/last_unread_message_test.dart
+++ test/modules/storage_manager/last_unread_message_test.dart
@@ -1,8 +1,9 @@
 import 'package:chat_app/modules/storage_manager/last_unread_message.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:chat_app/modules/storage_manager/preferences/user_preferences.dart';
 
 void main() async {
-  group('LastUnreadMessage', () {
+  group('LastUnreadMessageTrue', () {
     test('setUnreadMessage', () async {
       final unreadMessage = UnreadMessage();
 
@@ -12,6 +13,17 @@ void main() async {
       expect(lastUnreadMessage.lastUnreadMessageId, 232);
       expect(lastUnreadMessage.friendsChatId, 353);
       expect(lastUnreadMessage.isRead, true);
+    });
+
+    test('set unread false', () async {
+      await UnreadMessage().setUnreadMessage(34522542, 54, false);
+      final unreadStatus = await UserPreferences().getIsRead();
+      final messageId = await UserPreferences().getMessageId();
+      final friendsChatId = await UserPreferences().getFriendsChatId();
+
+      expect(unreadStatus, false);
+      expect(messageId, 34522542);
+      expect(friendsChatId, 54);
     });
   });
 }
