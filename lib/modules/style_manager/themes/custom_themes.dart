dc760e38f69c43326549f2fb7977e4f86dcc8854
anatoliyKaragodin <anatoliy.karagodin@gmail.com>
2022-12-09 16:30:24 +0400

    add custom colors to DeepPurpleTheme

--- lib/modules/style_manager/themes/custom_themes.dart
+++ lib/modules/style_manager/themes/custom_themes.dart
@@ -2,14 +2,36 @@ import 'package:flutter/material.dart';
 
 class CustomTheme {
   ThemeData lightThemeDeepPurple = ThemeData(
-    brightness: Brightness.light,
-    primarySwatch: Colors.deepPurple,
-  );
+      brightness: Brightness.light,
+      // primarySwatch: Colors.deepPurple,
+      colorScheme: ColorScheme(
+          brightness: Brightness.dark,
+          primary: Color(0xFF7E57C2),
+          onPrimary: Color(0xFF594280),
+          secondary: Colors.tealAccent.shade100,
+          onSecondary: Color(0xFFF44336),
+          error: Colors.redAccent,
+          onError: Colors.red,
+          background: Color(0xFFE0E0E0),
+          onBackground: Color(0xFFE0E0E0),
+          surface: Color(0xFFDBD8D8),
+          onSurface: Color(0xFFFFFFFF)));
   ThemeData darkThemeDeepPurple = ThemeData(
-    brightness: Brightness.dark,
-    primarySwatch: Colors.deepPurple,
-  );
-
+      brightness: Brightness.dark,
+      // primarySwatch: Colors.deepPurple,
+      //  primaryColor: Color(0xFF7E57C2),
+      colorScheme: ColorScheme(
+          brightness: Brightness.dark,
+          primary: Color(0xFF7E57C2),
+          onPrimary: Color(0xFF594280),
+          secondary: Colors.tealAccent.shade100,
+          onSecondary: Color(0xFFF44336),
+          error: Colors.redAccent,
+          onError: Colors.red,
+          background: Color(0xFFE0E0E0),
+          onBackground: Color(0xFFE0E0E0),
+          surface: Color(0xFFDBD8D8),
+          onSurface: Color(0xFFFFFFFF)));
 
   ThemeData darkThemeLightBlue = ThemeData(
     brightness: Brightness.dark,
@@ -19,7 +41,6 @@ class CustomTheme {
     brightness: Brightness.light,
     primarySwatch: Colors.lightBlue,
   );
-
 
   ThemeData darkThemeOrange = ThemeData(
     brightness: Brightness.dark,
