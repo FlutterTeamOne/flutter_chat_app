cb20cad384902ad57f4aec7513d25684e816c15f
x4dead <muratovargen0@gmail.com>
2022-12-12 01:35:11 +0600

    changed floating window, added small pagination

--- lib/ui/widgets/asap_page/widgets/app_card.dart
+++ lib/ui/widgets/asap_page/widgets/app_card.dart
@@ -1,4 +1,8 @@
-﻿import 'package:flutter/material.dart';
+﻿import 'dart:developer';
+import 'dart:ui';
+import 'package:custom_pop_up_menu/custom_pop_up_menu.dart';
+import 'package:flutter/material.dart';
+import 'list_tile_widget.dart';
 
 class AppCardWidget extends StatelessWidget {
   const AppCardWidget({
@@ -12,19 +16,55 @@ class AppCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
-        side: BorderSide(
-          width: 0.1,
-        ),
+    final items = [
+      // Тут находится то чо отображается в плавающем окне
+      ListTileWidget(
+        icon: Icons.edit,
+        text: 'Edit',
+        onTap: () {},
       ),
-      margin: EdgeInsets.only(left: 320, bottom: 5, top: 5, right: marginIndex),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        child: SelectableText(
-          message,
+      const SizedBox(height: 5),
+      ListTileWidget(
+        icon: Icons.delete,
+        text: 'Delete',
+        onTap: () {},
+      ),
+    ];
+    // Кастомное меню
+    return CustomPopupMenu(
+      showArrow: false,
+      position: PreferredPosition.bottom,
+      barrierColor: Colors.transparent,
+      enablePassEvent: false,
+      verticalMargin: -2,
+      pressType: PressType.singleClick,
+      menuBuilder: () => Container(
+        width: 160,
+        padding: const EdgeInsets.all(10),
+        decoration: const BoxDecoration(
+          color: Color(0xFF4C4C4C),
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(8),
+            bottomRight: Radius.circular(8),
+            topLeft: Radius.circular(8),
+          ),
+        ),
+        child: Column(children: items),
+      ),
+      child: Card(
+        elevation: 2,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+          side: const BorderSide(
+            width: 0.1,
+          ),
+        ),
+        margin: EdgeInsets.only(bottom: 5, top: 5, right: marginIndex),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          child: SelectableText(
+            message,
+          ),
         ),
       ),
     );
--- lib/ui/widgets/asap_page/widgets/message_cards/my_message_card.dart
+++ lib/ui/widgets/asap_page/widgets/message_cards/my_message_card.dart
@@ -9,84 +9,40 @@ class MyMessageCardWidget extends StatelessWidget {
   final int isSuccess;
   @override
   Widget build(BuildContext context) {
-    var items = [
-      PopupMenuItem(
-        onTap: () {
-          //
-        },
-        mouseCursor: MouseCursor.uncontrolled,
-        padding: const EdgeInsets.symmetric(horizontal: 12),
-        height: 38,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('Edit', style: Theme.of(context).textTheme.bodyMedium),
-            const Icon(Icons.edit, size: 18),
-          ],
-        ),
-      ),
-      PopupMenuItem(
-        onTap: () {
-          //
-        },
-        mouseCursor: MouseCursor.uncontrolled,
-        padding: const EdgeInsets.symmetric(horizontal: 12),
-        height: 38,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('Delete', style: Theme.of(context).textTheme.bodyMedium),
-            const Icon(Icons.delete, size: 18),
-          ],
-        ),
-      ),
-    ];
-
     final currentWidth = MediaQuery.of(context).size.width;
-    return PopupMenuButton(
-      tooltip: '',
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(10),
-            bottomRight: Radius.circular(10),
-            topLeft: Radius.circular(10)),
-      ),
-      position: PopupMenuPosition.under,
-      splashRadius: 0,
-      itemBuilder: (context) => items,
-      child: Align(
-        alignment: Alignment.centerRight,
-        child: ConstrainedBox(
-          constraints:
-              BoxConstraints(maxWidth: MediaQuery.of(context).size.width - 145),
-          child: isSuccess == 1
-              ? AppCardWidget(message: message, marginIndex: 15)
-              : Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    currentWidth > 888.8
-                        ? Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              AppCardWidget(marginIndex: 5, message: message),
-                              const Icon(
-                                Icons.error,
-                                //color: AppColor.colorF44336,
-                              )
-                            ],
-                          )
-                        : AppCardWidget(
-                            marginIndex: 10,
-                            message: message,
-                          ),
-                    const Text(
-                      'Not Delivered',
-                      // style: AppTextStyle.s14AbelGrey
-                      //     .copyWith(color: AppColor.colorF44336),
-                    )
-                  ],
-                ),
+    return Align(
+      alignment: Alignment.centerRight,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          maxWidth: currentWidth > 1150 ? 750 : 350,
         ),
+        child: isSuccess == 1
+            ? AppCardWidget(message: message, marginIndex: 15)
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  currentWidth > 888.8
+                      ? Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            AppCardWidget(marginIndex: 5, message: message),
+                            const Icon(
+                              Icons.error,
+                              //color: AppColor.colorF44336,
+                            )
+                          ],
+                        )
+                      : AppCardWidget(
+                          marginIndex: 10,
+                          message: message,
+                        ),
+                  const Text(
+                    'Not Delivered',
+                    // style: AppTextStyle.s14AbelGrey
+                    //     .copyWith(color: AppColor.colorF44336),
+                  )
+                ],
+              ),
       ),
     );
   }
