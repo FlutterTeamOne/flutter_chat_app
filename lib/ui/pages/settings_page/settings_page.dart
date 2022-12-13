eb97f2fb6983e9f03915ac169cff0d7f5a5506ab
SharovIvanY <SharovIvanY@gmail.com>
2022-12-13 15:54:10 +0300

    Practice with ListView.builder

--- lib/ui/pages/settings_page/settings_page.dart
+++ lib/ui/pages/settings_page/settings_page.dart
@@ -11,82 +11,111 @@ class SettingsPage extends StatelessWidget {
     return Scaffold(body: Center(child: themeSettings(context)));
   }
 
-  Center themeSettings(BuildContext context) {
+  // Center themeSettings(BuildContext context) {
 
-    return Center(
-      child: CustomScrollView(
+  //   return Center(
+  //     child: CustomScrollView(
+  //       scrollDirection: Axis.vertical,
+  //       slivers: [
+  //         SliverGrid(
+  //           gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+  //             maxCrossAxisExtent: 100.0,
+  //             mainAxisSpacing: 10.0,
+  //             crossAxisSpacing: 10.0,
+  //             childAspectRatio: 2.0,
+  //           ),
+  //           delegate: SliverChildBuilderDelegate(
+  //             (BuildContext context, int gridIndex) {
+  //               final ChangeThemeBloc changeThemeBloc = context.read<ChangeThemeBloc>();
+  //                return Card(
+  //                   shape: RoundedRectangleBorder(
+  //                   borderRadius: BorderRadius.circular(20.0)),
+  //                  color: changeThemeBloc.state.cardColor[gridIndex],
+  //                  child: IconButton(
+  //                    onPressed: () {
+  //                      changeThemeBloc.add(SetThemeEvent(index: gridIndex));
+  //                    },
+  //                    icon: Icon(
+  //                      changeThemeBloc.state.selectIcon[gridIndex],
+  //                      color: changeThemeBloc.iconColor[gridIndex],
+  //                    ),
+  //                    iconSize: 20,
+  //                  ),
+  //                );
+  //             },
+  //             childCount: 6,
+  //           ),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 
-        scrollDirection: Axis.vertical,
-        slivers: [
-          SliverGrid(
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 100.0,
-              mainAxisSpacing: 10.0,
-              crossAxisSpacing: 10.0,
-              childAspectRatio: 2.0,
-            ),
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int gridIndex) {
-                final ChangeThemeBloc changeThemeBloc = context.read<ChangeThemeBloc>();
-                 return Card(
-                    shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0)),
-                   color: changeThemeBloc.state.cardColor[gridIndex],
-                   child: IconButton(
-                     onPressed: () {
-                       changeThemeBloc.add(SetThemeEvent(index: gridIndex));
-                     },
-                     icon: Icon(
-                       changeThemeBloc.state.selectIcon[gridIndex],
-                       color: changeThemeBloc.iconColor[gridIndex],
-                     ),
-                     iconSize: 20,
-                   ),
-                 );
-              },
-              childCount: 6,
-            ),
-          ),
-        ],
-      ),
+  SizedBox themeSettings(BuildContext context) {
+    final ChangeThemeBloc changeThemeBloc = context.read<ChangeThemeBloc>();
+    return SizedBox(
+      width: 200,
+      height: 200,
+      child: Center(
+          child: Container(
+        color: Colors.red,
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                    itemCount: 3,
+                    itemBuilder: (BuildContext context, int index) {
+                      return CardWidget(context, index);
+                    }))
+          ],
+        ),
+      )),
     );
   }
 }
 
-//   SizedBox themeSettings(BuildContext context) {
-//     final ChangeThemeBloc changeThemeBloc = context.read<ChangeThemeBloc>();
-//     return SizedBox(
-//       height: 500,
-//       width: 500,
-//       child: ListView.builder(
-//           itemCount: 3,
-//           itemBuilder: (BuildContext context, int cardIndex) {
-//             return Card(
-//                 shape: RoundedRectangleBorder(
-//                     borderRadius: BorderRadius.circular(20.0)),
-//                 color: changeThemeBloc.state.cardColor[cardIndex],
-//                 child: Wrap(children: [
-//                   ListView.builder(
-//                       scrollDirection: Axis.horizontal,
-//                       itemCount: 2,
-//                       itemBuilder:
-//                           (BuildContext context, int iconIndex) {
-//                         return IconButton(
-//                           onPressed: () {
-//                             changeThemeBloc.add(SetThemeEvent(
-//                                 activeElementIndex:
-//                                     cardIndex * 2 + iconIndex));
-//                           },
-//                           icon: Icon(
-//                             changeThemeBloc.state.selectIcon[iconIndex],
-//                             color: changeThemeBloc.state.iconColor[
-//                                 changeThemeBloc.state.elementIndex],
-//                           ),
-//                           iconSize: 20,
-//                         );
-//                       }),
-//                 ]));
-//           }),
-//     );
-//   }
-// }
+Widget CardWidget(BuildContext context, int index) {
+  final ChangeThemeBloc changeThemeBloc = context.read<ChangeThemeBloc>();
+  return Card(
+    color: Colors.black,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(20.0),
+    ),
+    child: Row(children: [
+      Expanded(
+          child: Icon(
+        changeThemeBloc.state.selectIcon[index],
+        color: Colors.red,
+      ))
+    ]),
+  );
+}
+        // child: ListView.builder(
+        //     itemCount: 3,
+        //     itemBuilder: (BuildContext context, int cardIndex) {
+              // return Card(
+              //     shape: RoundedRectangleBorder(
+              //         borderRadius: BorderRadius.circular(20.0)),
+              //     color: changeThemeBloc.state.cardColor[cardIndex],
+              //     child: Wrap(children: [
+              //       ListView.builder(
+              //           scrollDirection: Axis.horizontal,
+              //           itemCount: 2,
+              //           itemBuilder: (BuildContext context, int iconIndex) {
+              //             return IconButton(
+              //               onPressed: () {
+              //                 // changeThemeBloc.add(SetThemeEvent(
+              //                 //     activeElementIndex: cardIndex * 2 + iconIndex));
+              //               },
+              //               icon: Icon(
+              //                 changeThemeBloc.state.selectIcon[iconIndex],
+              //                 color: Colors.red,
+              //                 // changeThemeBloc.state
+              //                 //     .iconColor[changeThemeBloc.state.elementIndex],
+              //               ),
+              //               iconSize: 20,
+              //             );
+              //           }),
+              //     ]));
+            // }),
+
