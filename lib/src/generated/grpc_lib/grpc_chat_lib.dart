be4a2c2c3e947050e12e5d4d69339da0bd066deb
forswow <f0rsw0w@yandex.ru>
2022-12-14 18:23:12 +0300

    fix export

---
+++ lib/src/generated/grpc_lib/grpc_chat_lib.dart
@@ -0,0 +1,4 @@
+export 'package:chat_app/src/generated/chats/chats.pb.dart';
+export 'package:chat_app/src/generated/chats/chats.pbenum.dart';
+export 'package:chat_app/src/generated/chats/chats.pbgrpc.dart';
+export 'package:chat_app/src/generated/chats/chats.pbjson.dart';
--- lib/src/generated/grpc_lib/grpc_message_lib.dart
+++ lib/src/generated/grpc_lib/grpc_message_lib.dart
@@ -1,1 +1,4 @@
-export 'package:flutter';
+export 'package:chat_app/src/generated/messages/messages.pb.dart';
+export 'package:chat_app/src/generated/messages/messages.pbenum.dart';
+export 'package:chat_app/src/generated/messages/messages.pbgrpc.dart';
+export 'package:chat_app/src/generated/messages/messages.pbjson.dart';
