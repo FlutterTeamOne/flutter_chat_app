6ceacd9e4f88d38b664838b3e54eb6b9f6f2ab5b
anatoliyKaragodin <anatoliy.karagodin@gmail.com>
2022-12-14 17:52:32 +0400

    add new_user_widget.dart

--- lib/ui/pages/authentication_page/authentication_page.dart
+++ lib/ui/pages/authentication_page/authentication_page.dart
@@ -1,87 +1,14 @@
+import 'package:chat_app/ui/widgets/authentication%20page/widgets/new_user_widget.dart';
 import 'package:flutter/material.dart';
 
 class AuthenticationPage extends StatelessWidget {
   const AuthenticationPage({Key? key}) : super(key: key);
-  static const double textFieldWidth = 200;
-  static const double textPadding = 15;
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
-            children: [
-              const SizedBox(
-                height: 200,
-              ),
-              Row(
-                children: [
-                  Column(
-                    children: [
-                      SizedBox(
-                        width: 300,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: const [
-                            Padding(
-                              padding: EdgeInsets.only(right: textPadding),
-                              child: Text('Name'),
-                            ),
-                            SizedBox(width: textFieldWidth, child: TextField()),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        width: 300,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: const [
-                            Padding(
-                              padding: EdgeInsets.only(right: textPadding),
-                              child: Text('Email'),
-                            ),
-                            SizedBox(width: textFieldWidth, child: TextField()),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        width: 300,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: const [
-                            Padding(
-                              padding: EdgeInsets.only(right: textPadding),
-                              child: Text('Password'),
-                            ),
-                            SizedBox(width: textFieldWidth, child: TextField()),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 100.0),
-                        child: ElevatedButton(
-                            style: ButtonStyle(
-                                shape: MaterialStateProperty.all<
-                                        RoundedRectangleBorder>(
-                                    RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20.0),
-                            ))),
-                            onPressed: () {},
-                            child: const Text('Create new user')),
-                      ),
-                    ],
-                  ),
-                ],
-              )
-            ],
-          ),
-        ],
-      ),
+    return const Scaffold(
+      body: NewUserWidget(),
     );
   }
 }
