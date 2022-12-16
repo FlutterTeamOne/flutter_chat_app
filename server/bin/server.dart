ec93ccf3ef70098b965be148a09f0d71bc7a93d0
SharovIvanY <SharovIvanY@gmail.com>
2022-12-16 11:27:18 +0300

    fixed fail compile

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -217,54 +217,6 @@ class GrpcChats extends GrpcChatsServiceBase {
   }
 }
 
-class GrpcChats extends GrpcChatsServiceBase {
-  @override
-  Future<CreateChatResponse> createChat(
-      ServiceCall call, CreateChatRequest request) async {
-    var src = await ChatsServices().createChat(
-        friend1_id: request.friend1Id, friend2_id: request.friend1Id);
-    var createChatResponse = CreateChatResponse();
-    if (src[0]['chat_id'] != 0) {
-      createChatResponse.id = src[0]['chat_id'];
-      createChatResponse.createdDate = DateTime.now().toIso8601String();
-    }
-    return createChatResponse;
-  }
-
-  @override
-  Future<DeleteChatResponse> deleteChat(
-      ServiceCall call, DeleteChatRequest request) async {
-    var deleteResponse = DeleteChatResponse();
-    var src = await ChatsServices().deleteChat(id: request.id);
-    if (src != 0) {
-      deleteResponse.dateDeleted = DateTime.now().toIso8601String();
-    }
-    return deleteResponse;
-  }
-
-  @override
-  Future<GetChatResponse> getChat(
-      ServiceCall call, GetChatRequest request) async {
-    var getChatResp = GetChatResponse();
-    var src = await ChatsServices().getChatById(id: request.id);
-
-    if (src[0]['user_id'] != 0 && src[0]['user_id'] != null) {
-      getChatResp.friend1Id = src[0]['friend1_id'];
-      getChatResp.friend2Id = src[0]['friend2_id'];
-      getChatResp.createdDate = DateTime.now().toIso8601String();
-    }
-    return getChatResp;
-  }
-
-  @override
-  Future<UpdateChatResponse> updateChat(
-      ServiceCall call, UpdateChatRequest request) async {
-    var updateChatResp = UpdateChatResponse();
-
-    return updateChatResp;
-  }
-}
-
 class GrpcUsers extends GrpcUsersServiceBase {
   @override
   Future<CreateUserResponse> createUser(
