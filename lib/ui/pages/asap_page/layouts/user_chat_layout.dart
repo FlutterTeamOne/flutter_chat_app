5bc15004a7298961717c134d99a248d15a185dce
anatoliyKaragodin <anatoliy.karagodin@gmail.com>
2022-12-22 12:30:25 +0400

    add no messages for deleted user

--- lib/ui/pages/asap_page/layouts/user_chat_layout.dart
+++ lib/ui/pages/asap_page/layouts/user_chat_layout.dart
@@ -41,7 +41,40 @@ class UserChatLayoutState extends State<UserChatLayout> {
         TextInputWidget(
           onSubmitted: (text) => _sendAndChange(messageBloc),
           controller: controller,
-          onTap: () => _sendAndChange(messageBloc),
+          onTap: () => user.deletedDate!.isNotEmpty
+              ? showDialog(context: context, builder: (BuildContext context) {
+                return Dialog(
+                  shape:
+                      RoundedRectangleBorder(
+                        borderRadius:
+                        BorderRadius.circular(20.0),
+                      ),
+                  child: SizedBox(
+                  height: 80,
+                  width: 80,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('User ${user.name} is deleted'),
+                      ),
+
+                      ElevatedButton(
+                          style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius:
+                                    BorderRadius.circular(20.0),
+                                  ))),
+                          onPressed: () {
+                        Navigator.pop(context);
+                      }, child: Icon(Icons.close_rounded,))
+                    ],
+                  ),
+                ),);
+          })
+              : _sendAndChange(messageBloc),
           editState: messageBloc.state.editState,
           editText: controller.text,
           cancelEdit: () {
