e6bbf48668b4593b44ec6053b15113a255fe082a
anatoliyKaragodin <anatoliy.karagodin@gmail.com>
2022-12-22 16:08:40 +0400

    add load avatar from url dialog

--- lib/ui/pages/profile_page/widgets/app_dialog.dart
+++ lib/ui/pages/profile_page/widgets/app_dialog.dart
@@ -39,21 +39,53 @@ class _AppDialog extends StatelessWidget {
                 _LoadMethod(onTap: () {
                   showDialog(context: context, builder: (BuildContext context) {
                     TextEditingController newUrlController = TextEditingController();
-                    return Dialog(child: Column(children: [
-                      Text('Insert image link'),
-                      TextField(
-                        controller: newUrlController = TextEditingController(),
-                      ),
-                      ElevatedButton(onPressed: () {
-                        var newUrl = newUrlController.text;
-                        print(newUrl);
-                        Navigator.pop(context);}, child: Icon(Icons.check))
-                    ],),);
+                    return SizedBox(
+                      height: 100,
+                      width: 200,
+                      child: Dialog(
+                        shape:
+                            RoundedRectangleBorder(
+                              borderRadius:
+                              BorderRadius.circular(20.0),
+                            ),
+                        child: Column(children: [
+                        Text('Insert image link'),
+                        TextField(
+                          controller: newUrlController = TextEditingController(),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                              style: ButtonStyle(
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                        borderRadius:
+                                        BorderRadius.circular(20.0),
+                                      ))),
+                              onPressed: () {
+                            var newUrl = newUrlController.text;
+                            print(newUrl);
+                            Navigator.pop(context);}, child: Icon(Icons.check)),
+                        )
+                      ],),),
+                    );
                   });
                 }, text: 'Load image from url'),
               ],
             ),
-            Center(child: ElevatedButton(onPressed: (){Navigator.pop(context);}, child: Icon(Icons.close_rounded)))
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(child: ElevatedButton(
+                  style: ButtonStyle(
+                      shape: MaterialStateProperty.all<
+                          RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius:
+                            BorderRadius.circular(20.0),
+                          ))),
+                  onPressed: (){Navigator.pop(context);}, child: Icon(Icons.close_rounded))),
+            )
           ],
         ),
       ),
