0968db4ec664bc2acb2d3037fd37e500530197f6
Farkhat <faradzhalelov@gmail.ru>
2022-12-29 15:25:25 +0400

    add no such id in add chat

--- lib/ui/pages/profile_page/widgets/layout/profile_layout.dart
+++ lib/ui/pages/profile_page/widgets/layout/profile_layout.dart
@@ -16,29 +16,29 @@ class _ProfileLayout extends StatelessWidget {
     }
     return ListView(
       children: [
-        // Фон и аватарка
-        Stack(
-          children: [
-            const SizedBox(height: 205),
-            // Фон
-            _AppBluredImage(image: userMain.profilePicLink),
-            // Аватарка
-            _UserPic(userPic: userMain.profilePicLink),
-            // Кнопка изменение аву
-            const _ChangeUserPic(),
+        // // Фон и аватарка
+        // Stack(
+        //   children: [
+        //     const SizedBox(height: 205),
+        //     // Фон
+        //     _AppBluredImage(image: userMain.profilePicLink),
+        //     // Аватарка
+        //     _UserPic(userPic: userMain.profilePicLink),
+        //     // Кнопка изменение аву
+        //     const _ChangeUserPic(),
 
-            IconButton(
-              onPressed: () async {
-                context.read<UserBloc>().add(ChangeUserEvent(isStartDB: true));
-                context.read<UserBloc>().add(ReadUsersEvent());
-                //закрыть базу
-                await DBHelper.instanse.close();
-                Navigator.of(context).pop();
-              },
-              icon: const Icon(Icons.arrow_back),
-            ),
-          ],
-        ),
+        //     IconButton(
+        //       onPressed: () async {
+        //         context.read<UserBloc>().add(ChangeUserEvent(isStartDB: true));
+        //         context.read<UserBloc>().add(ReadUsersEvent());
+        //         //закрыть базу
+        //         await DBHelper.instanse.close();
+        //         Navigator.of(context).pop();
+        //       },
+        //       icon: const Icon(Icons.arrow_back),
+        //     ),
+        //   ],
+        // ),
         // Остальное
         Padding(
           padding: const EdgeInsets.all(15),
@@ -48,124 +48,117 @@ class _ProfileLayout extends StatelessWidget {
               // Фон и аватарка
               Stack(
                 children: [
-                  // Фон и аватарка
-                  Stack(
-                    children: [
-                      const SizedBox(height: 205),
-                      // Фон
-                      _AppBluredImage(image: userMain.profilePicLink),
-                      // Аватарка
-                      _UserPic(userPic: userMain.profilePicLink),
-                      // Кнопка изменение аву
-                      const _ChangeUserPic(),
-                      IconButton(
-                        onPressed: () async {
-                          // context.read<ChatBloc>().close();
-                          //закрыть базу
-                          context
-                              .read<UserBloc>()
-                              .add(ChangeUserEvent(isStartDB: true));
-                          context.read<UserBloc>().add(ReadUsersEvent());
-                          context.read<ChatBloc>().add(GetChatIdEvent(-1));
-                          await DBHelper.instanse.close();
-                          Future.delayed(Duration(seconds: 1),
-                              () => Navigator.of(context).pushNamed('/'));
-                        },
-                        icon: Icon(Icons.arrow_back),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Username',
-                          style: Theme.of(context).textTheme.headline6,
-                        ),
-                        const SizedBox(height: 2),
-                        Row(
-                          children: [
-                            Text(
-                              userMain.name ?? 'unknow',
-                              style: Theme.of(context).textTheme.bodyText2,
-                            ),
-                            // Кнопка смены имени
-                            ButtonChangeName(userMain: userMain)
-                          ],
-                        ),
-                        const SizedBox(height: 10),
-                        Text(
-                          'Email',
-                          style: Theme.of(context).textTheme.headline6,
-                        ),
-                        const SizedBox(height: 2),
-                        Row(
-                          children: [
-                            Text(
-                              userMain.email ?? '???',
-                              style: Theme.of(context).textTheme.bodyText2,
-                            ),
-                            ButtonChangeEmail(userMain: userMain)
-                          ],
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          children: [
-                            ElevatedButton(
-                                style: ButtonStyle(
-                                    shape: MaterialStateProperty.all<
-                                            RoundedRectangleBorder>(
-                                        RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20.0),
-                                ))),
-                                onPressed: () {
-                                  context.read<UserBloc>().add(
-                                      DeleteUserEvent(userId: userMain.userId));
-                                  print(userMain.userId);
-                                  showDialog(
-                                      context: context,
-                                      builder: (BuildContext context) {
-                                        final userIsDeleted = context
-                                            .watch<UserBloc>()
-                                            .state
-                                            .isDeleted;
-                                        return Dialog(
-                                          shape: RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(20.0),
-                                          ),
-                                          child: SizedBox(
-                                            height: 80,
-                                            width: 50,
-                                            child: Column(
-                                              children: [
-                                                Padding(
-                                                    padding:
-                                                        EdgeInsets.all(8.0),
-                                                    child: userIsDeleted ==
-                                                            false
-                                                        ? Text(
-                                                            'User ${userMain.name} is not deleted')
-                                                        : Text(
-                                                            'User ${userMain.name} is deleted')),
-                                                DeleteDialogWidget()
-                                              ],
-                                            ),
-                                          ),
-                                        );
-                                      });
-                                },
-                                child: Text('Delete user')),
-                          ],
-                        ),
-                      ],
-                    ),
+                  const SizedBox(height: 205),
+                  // Фон
+                  _AppBluredImage(image: userMain.profilePicLink),
+                  // Аватарка
+                  _UserPic(userPic: userMain.profilePicLink),
+                  // Кнопка изменение аву
+                  const _ChangeUserPic(),
+                  IconButton(
+                    onPressed: () async {
+                      // context.read<ChatBloc>().close();
+                      //закрыть базу
+                      context
+                          .read<UserBloc>()
+                          .add(ChangeUserEvent(isStartDB: true));
+                      context.read<UserBloc>().add(ReadUsersEvent());
+                      context.read<ChatBloc>().add(GetChatIdEvent(-1));
+                      await DBHelper.instanse.close();
+                      Future.delayed(Duration(seconds: 1),
+                          () => Navigator.of(context).pushNamed('/'));
+                    },
+                    icon: Icon(Icons.arrow_back),
                   ),
                 ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Username',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    const SizedBox(height: 2),
+                    Row(
+                      children: [
+                        Text(
+                          userMain.name ?? 'unknow',
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                        // Кнопка смены имени
+                        ButtonChangeName(userMain: userMain)
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Email',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    const SizedBox(height: 2),
+                    Row(
+                      children: [
+                        Text(
+                          userMain.email ?? '???',
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                        ButtonChangeEmail(userMain: userMain)
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        ElevatedButton(
+                            style: ButtonStyle(
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ))),
+                            onPressed: () {
+                              context.read<UserBloc>().add(
+                                  DeleteUserEvent(userId: userMain.userId));
+                              print(userMain.userId);
+                              showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    final userIsDeleted = context
+                                        .watch<UserBloc>()
+                                        .state
+                                        .isDeleted;
+                                    return Dialog(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(20.0),
+                                      ),
+                                      child: SizedBox(
+                                        height: 80,
+                                        width: 50,
+                                        child: Column(
+                                          children: [
+                                            Padding(
+                                                padding: EdgeInsets.all(8.0),
+                                                child: userIsDeleted == false
+                                                    ? Text(
+                                                        'User ${userMain.name} is not deleted')
+                                                    : Text(
+                                                        'User ${userMain.name} is deleted')),
+                                            DeleteDialogWidget()
+                                          ],
+                                        ),
+                                      ),
+                                    );
+                                  });
+                            },
+                            child: Text('Delete user')),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
