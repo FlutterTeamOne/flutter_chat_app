9337a7247499b3c02c1183d7e764eb30da00fbba
bukhanovskayamarina22 <116250199+bukhanovskayamarina22@users.noreply.github.com>
2023-01-05 10:33:25 +0300

    local db: "create table" queries tests

--- test/modules/storage_manager/db_helper/local_db_helper_test.dart
+++ test/modules/storage_manager/db_helper/local_db_helper_test.dart
@@ -62,4 +62,358 @@ Future main() async {
     //   expect(r, [{'name': 'user_last_time_online'}]);
     // });
   });
+
+  group('Local db: "CREATE TABLE" queries:', () {
+    setUpAll(() {
+      sqfliteFfiInit();
+      databaseFactory = databaseFactoryFfi;
+    });
+
+    test('CREATE TABLE users', () async {
+      databaseFactory.deleteDatabase('test');
+      var db = await databaseFactory.openDatabase('test');
+      await db.transaction((txn) async {
+        await txn.execute('''
+        CREATE TABLE ${DatabaseConst.userTable} (
+          ${DatabaseConst.usersColumnUserId} ${DatabaseConst.integer} ${DatabaseConst.primaryKey} ${DatabaseConst.autoincrement},
+          ${DatabaseConst.usersColumnName} ${DatabaseConst.char50} ${DatabaseConst.notNull},
+          ${DatabaseConst.usersColumnEmail} ${DatabaseConst.char50} ${DatabaseConst.notNull},
+          ${DatabaseConst.usersColumnProfilePicLink} ${DatabaseConst.char50} ${DatabaseConst.notNull},
+          ${DatabaseConst.usersColumnCreatedDate} ${DatabaseConst.char26} ${DatabaseConst.notNull},
+          ${DatabaseConst.usersColumnUpdatedDate} ${DatabaseConst.char26} ${DatabaseConst.notNull},
+          ${DatabaseConst.usersColumnDeletedDate} ${DatabaseConst.char26}
+          )
+      ''');
+      });
+
+      var r = await db.rawQuery('''PRAGMA table_info(users)''');
+      var matcher = [
+            {
+              'cid': 0,
+              'name': 'user_id',
+              'type': 'INTEGER',
+              'notnull': 0,
+              'dflt_value': null,
+              'pk': 1
+            },
+            {
+              'cid': 1,
+              'name': 'name',
+              'type': 'char(50)',
+              'notnull': 1,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 2,
+              'name': 'email',
+              'type': 'char(50)',
+              'notnull': 1,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 3,
+              'name': 'profile_pic_link',
+              'type': 'char(50)',
+              'notnull': 1,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 4,
+              'name': 'created_date',
+              'type': 'char(26)',
+              'notnull': 1,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 5,
+              'name': 'updated_date',
+              'type': 'char(26)',
+              'notnull': 1,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 6,
+              'name': 'deleted_date',
+              'type': 'char(26)',
+              'notnull': 0,
+              'dflt_value': null,
+              'pk': 0
+            }
+          ];
+      expect(r, matcher);
+      db.close();
+    });
+
+    test('CREATE TABLE chats', () async {
+      databaseFactory.deleteDatabase('test');
+      var db = await databaseFactory.openDatabase('test');
+      await db.transaction((txn) async {
+      await txn.execute('''
+        CREATE TABLE ${DatabaseConst.chatsTable}(
+          ${DatabaseConst.chatsColumnChatId} ${DatabaseConst.integer} ${DatabaseConst.primaryKey} ${DatabaseConst.autoincrement},
+          ${DatabaseConst.chatsColumnUserId} ${DatabaseConst.integer} ${DatabaseConst.notNull} ${DatabaseConst.unique},
+          ${DatabaseConst.chatsColumnCreatedDate} ${DatabaseConst.char26} ${DatabaseConst.notNull},
+          ${DatabaseConst.chatsColumnUpdatedDate} ${DatabaseConst.char26},
+          ${DatabaseConst.chatsColumnDeletedDate} ${DatabaseConst.char26},
+          ${DatabaseConst.constraint} CHATS_FK_84 ${DatabaseConst.foreignKey} ( ${DatabaseConst.chatsColumnUserId} ) ${DatabaseConst.references} ${DatabaseConst.userTable} ( ${DatabaseConst.usersColumnUserId} )
+        )
+        ''');
+      });
+
+      var r = await db.rawQuery('''PRAGMA table_info(chats)''');
+      var matcher = [
+            {
+              'cid': 0,
+              'name': 'chat_id',
+              'type': 'INTEGER',
+              'notnull': 0,
+              'dflt_value': null,
+              'pk': 1
+            },
+            {
+              'cid': 1,
+              'name': 'user_id',
+              'type': 'INTEGER',
+              'notnull': 1,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 2,
+              'name': 'created_date',
+              'type': 'char(26)',
+              'notnull': 1,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 3,
+              'name': 'update_date',
+              'type': 'char(26)',
+              'notnull': 0,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 4,
+              'name': 'deleted_date',
+              'type': 'char(26)',
+              'notnull': 0,
+              'dflt_value': null,
+              'pk': 0
+            }
+          ];
+      expect(r, matcher);
+      db.close();
+    });
+
+    test('CREATE TABLE messages', () async {
+      databaseFactory.deleteDatabase('test');
+      var db = await databaseFactory.openDatabase('test');
+      await db.transaction((txn) async {
+      await txn.execute('''
+        CREATE TABLE ${DatabaseConst.messageTable} (
+          ${DatabaseConst.messagesColumnLocalMessagesId} ${DatabaseConst.integer} ${DatabaseConst.primaryKey} ${DatabaseConst.autoincrement},
+          ${DatabaseConst.messagesColumnChatId} ${DatabaseConst.integer} ${DatabaseConst.notNull},
+          ${DatabaseConst.messagesColumnCreatedDate} ${DatabaseConst.char26} ${DatabaseConst.notNull},
+          ${DatabaseConst.messagesColumnSenderId} ${DatabaseConst.integer} ${DatabaseConst.notNull},
+          ${DatabaseConst.messagesColumnMessageId} ${DatabaseConst.integer},
+          ${DatabaseConst.messagesColumnIsRead} ${DatabaseConst.integer} ${DatabaseConst.notNull} DEFAULT 0,
+          ${DatabaseConst.messagesColumnContent} ${DatabaseConst.char50} ${DatabaseConst.notNull},
+          ${DatabaseConst.messagesColumnUpdatedDate} ${DatabaseConst.char26} ${DatabaseConst.notNull},
+          ${DatabaseConst.messagesColumnDeletedDate} ${DatabaseConst.char26}, 
+          ${DatabaseConst.messagesColumnAttachmentId} ${DatabaseConst.integer},
+          ${DatabaseConst.messagesColumnContentType} ${DatabaseConst.text},
+          ${DatabaseConst.constraint} MESSAGES_FK_79 ${DatabaseConst.foreignKey} ( ${DatabaseConst.messagesColumnChatId} ) ${DatabaseConst.references} ${DatabaseConst.chatsTable} ( ${DatabaseConst.chatsColumnChatId} ),
+
+          ${DatabaseConst.constraint} MESSAGES_FK_80 ${DatabaseConst.foreignKey} ( ${DatabaseConst.messagesColumnSenderId} ) ${DatabaseConst.references} ${DatabaseConst.userTable} ( ${DatabaseConst.usersColumnUserId} ),
+
+          ${DatabaseConst.constraint} MESSAGES_FK_81 ${DatabaseConst.foreignKey} ( ${DatabaseConst.messagesColumnAttachmentId} ) ${DatabaseConst.references} ${DatabaseConst.attachmentsTable} ( ${DatabaseConst.attachmentsColumnAttachmentId} ),
+
+          CHECK ((is_read = 0) OR (is_read = 1)),
+          CHECK (LENGTH(${DatabaseConst.messagesColumnCreatedDate}) = 26)
+        )
+        ''');
+      });
+
+      var r = await db.rawQuery('''PRAGMA table_info(messages)''');
+      var matcher = [
+            {
+              'cid': 0,
+              'name': 'local_messages_id',
+              'type': 'INTEGER',
+              'notnull': 0,
+              'dflt_value': null,
+              'pk': 1
+            },
+            {
+              'cid': 1,
+              'name': 'chat_id',
+              'type': 'INTEGER',
+              'notnull': 1,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 2,
+              'name': 'created_date',
+              'type': 'char(26)',
+              'notnull': 1,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 3,
+              'name': 'sender_id',
+              'type': 'INTEGER',
+              'notnull': 1,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 4,
+              'name': 'message_id',
+              'type': 'INTEGER',
+              'notnull': 0,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 5,
+              'name': 'is_read',
+              'type': 'INTEGER',
+              'notnull': 1,
+              'dflt_value': '0',
+              'pk': 0
+            },
+            {
+              'cid': 6,
+              'name': 'content',
+              'type': 'char(50)',
+              'notnull': 1,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 7,
+              'name': 'updated_date',
+              'type': 'char(26)',
+              'notnull': 1,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 8,
+              'name': 'deleted_date',
+              'type': 'char(26)',
+              'notnull': 0,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 9,
+              'name': 'attachment_id',
+              'type': 'INTEGER',
+              'notnull': 0,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 10,
+              'name': 'content_type',
+              'type': 'TEXT',
+              'notnull': 0,
+              'dflt_value': null,
+              'pk': 0
+            }
+          ];
+      expect(r, matcher);
+      db.close();
+    });
+
+    test('CREATE TABLE main_user', () async {
+      databaseFactory.deleteDatabase('test');
+      var db = await databaseFactory.openDatabase('test');
+      await db.transaction((txn) async {
+      await txn.execute('''
+        CREATE TABLE ${DatabaseConst.mainUserTable}(
+          ${DatabaseConst.mainUserColumnUserId} ${DatabaseConst.integer},
+          ${DatabaseConst.mainUserColumnKey} ${DatabaseConst.char50},
+          ${DatabaseConst.mainUserColumnDataSync} ${DatabaseConst.char26}
+        )
+        ''');
+      });
+
+      var r = await db.rawQuery('''PRAGMA table_info(main_user)''');
+      var matcher = [
+            {
+              'cid': 0,
+              'name': 'user_id',
+              'type': 'INTEGER',
+              'notnull': 0,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 1,
+              'name': 'user_key',
+              'type': 'char(50)',
+              'notnull': 0,
+              'dflt_value': null,
+              'pk': 0
+            },
+            {
+              'cid': 2,
+              'name': 'date_sync',
+              'type': 'char(26)',
+              'notnull': 0,
+              'dflt_value': null,
+              'pk': 0
+            }
+          ];
+      expect(r, matcher);
+      db.close();
+    });
+
+    test('CREATE TABLE attachments', () async {
+      databaseFactory.deleteDatabase('test');
+      var db = await databaseFactory.openDatabase('test');
+      await db.transaction((txn) async {
+      await txn.execute('''
+        CREATE TABLE ${DatabaseConst.attachmentsTable} 
+        (
+        ${DatabaseConst.attachmentsColumnAttachmentId} ${DatabaseConst.integer} ${DatabaseConst.primaryKey} ${DatabaseConst.autoincrement},
+        ${DatabaseConst.attachmentsColumnAttachmentMeta} ${DatabaseConst.char4096} ${DatabaseConst.notNull}
+      )
+      ''');
+      });
+
+      var r = await db.rawQuery('''PRAGMA table_info(attachments)''');
+      var matcher = [
+            {
+              'cid': 0,
+              'name': 'attachment_id',
+              'type': 'INTEGER',
+              'notnull': 0,
+              'dflt_value': null,
+              'pk': 1
+            },
+            {
+              'cid': 1,
+              'name': 'attachment_meta',
+              'type': 'char(4096)',
+              'notnull': 1,
+              'dflt_value': null,
+              'pk': 0
+            }
+          ];
+      expect(r, matcher);
+      db.close();
+    });
+  });
 }
