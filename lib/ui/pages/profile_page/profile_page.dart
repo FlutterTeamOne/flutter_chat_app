9802f3621489c378adf35258add83245f4f6d4d8
x4dead <muratovargen0@gmail.com>
2023-01-09 18:40:00 +0600

    extract exit_button

--- lib/ui/pages/profile_page/profile_page.dart
+++ lib/ui/pages/profile_page/profile_page.dart
@@ -1,16 +1,11 @@
 import 'package:blur/blur.dart';
 import 'package:chat_app/modules/signal_service/river/chat_ref/chat_notifier.dart';
 import 'package:chat_app/modules/signal_service/river/user_ref/user_notifier.dart';
-import '../../../modules/signal_service/library/library_signal_service.dart';
 import '../../../modules/signal_service/river/river.dart';
-import '../../../modules/storage_manager/db_helper/db_helper.dart';
 import '../../../modules/storage_manager/db_helper/user_path.dart';
 import '../../../src/libraries/library_all.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../../auth/authorization_page.dart';
 
 part 'widgets/change_user_pic.dart';
 part 'widgets/load_method.dart';
@@ -18,6 +13,7 @@ part 'widgets/app_blured_image.dart';
 part 'widgets/_user_picture.dart';
 part 'widgets/app_dialog.dart';
 part 'widgets/layout/profile_layout.dart';
+part 'widgets/exit_button.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({
--- lib/ui/pages/profile_page/widgets/layout/profile_layout.dart
+++ lib/ui/pages/profile_page/widgets/layout/profile_layout.dart
@@ -270,45 +270,6 @@ class _ProfileLayout extends StatelessWidget {
   }
 }
 
-class ExitButton extends StatelessWidget {
-  const ExitButton({
-    Key? key,
-    required this.userPod,
-    required this.chatPod,
-  }) : super(key: key);
-
-  final UserNotifier userPod;
-  final ChatNotifier chatPod;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical:16.0,horizontal: 16),
-      child: IconButton(
-          onPressed: () async {
-            // context.read<ChatBloc>().close();
-            //закрыть базу
-            userPod.changeUser(true);
-            userPod.readUser();
-            chatPod.getChatId(-1);
-            await DBHelper.instanse.close();
-            Future.delayed(const Duration(seconds: 1),
-                () => Navigator.of(context).pushNamed('/'));
-          },
-          icon: const Icon(Icons.exit_to_app),
-          style: const ButtonStyle(
-            backgroundColor: MaterialStatePropertyAll(Colors.white),
-            padding: MaterialStatePropertyAll(EdgeInsets.all(10)),
-            shape: MaterialStatePropertyAll(RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-              topRight: Radius.circular(20),
-              bottomLeft: Radius.circular(20),
-            ))),
-          )),
-    );
-  }
-}
-
 class ButtonChangeEmail extends ConsumerWidget {
   const ButtonChangeEmail({
     Key? key,
