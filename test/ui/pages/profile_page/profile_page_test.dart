969460a9f9f6670ce32a26e0d1f2ea9e3ee78706
slavlen <66310017+slavlen@users.noreply.github.com>
2023-01-10 12:48:07 +0300

    Update profile_page_test.dart
    по красоте

--- test/ui/pages/profile_page/profile_page_test.dart
+++ test/ui/pages/profile_page/profile_page_test.dart
@@ -1,57 +1,80 @@
 import 'package:chat_app/domain/data/dto/user_dto/user_dto.dart';
-import 'package:chat_app/main.dart';
 import 'package:chat_app/modules/signal_service/river/river.dart';
+import 'package:chat_app/modules/signal_service/river/user_ref/user_notifier.dart';
 import 'package:chat_app/modules/signal_service/river/user_ref/user_state_ref.dart';
 import 'package:chat_app/ui/pages/profile_page/profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
+
+final userPodFake = StateProvider((ref) => UserStateRef());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Consumer(
-      builder: (context, ref, _) => ref.read(userPod).users?[0].name != null
-          ? const ProfileLayout(key: Key('ProfileLayout'))
-          : const Center(
-              child: CircularProgressIndicator(key: Key('CircularProfilePage')),
-            ),
+      builder: (context, ref, _) =>
+          ref.read(userPodFake).users?[0].name != null &&
+                  ref.read(userPodFake).users?[0].name != ''
+              ? const Text('a')
+              : const Center(
+                  child: CircularProgressIndicator(),
+                ),
     ));
   }
 }
 
-final userPod = StateProvider<UserStateRef>((ref) => UserStateRef(users: [
-      UserDto(
-          name: 'TestName',
-          email: '',
-          createdDate: '',
-          profilePicLink: '',
-          updatedDate: '')
-    ], userDb: false, isDeleted: null));
-
-main() {
-  group('test profile_page', () {
-    testWidgets('Если нет базы юзеров', (tester) async {
-      // final userPod = StateProvider<UserStateRef>(
-      //     (ref) => UserStateRef(users: null, userDb: null, isDeleted: null));
+void main() {
+  group("Отрисовка виджетов на ProfilePage", () {
+    testWidgets('Отрисовка Consumer', (tester) async {
       await tester.pumpWidget(ProviderScope(child: MyApp()));
-      expect(find.byKey(ValueKey('ProfileLayout')), findsNothing);
-      expect(find.byKey(ValueKey('CircularProfilePage')), findsOneWidget);
+      expect(find.byType(Consumer), findsOneWidget);
     });
 
-    testWidgets('Если есть юзер, то отрисовать профиль', (tester) async {
-      var user = UserDto(
-          name: 'TestName',
-          email: '',
-          createdDate: '',
-          profilePicLink: '',
-          updatedDate: '');
-      final userPod = StateProvider<UserStateRef>(
-          (ref) => UserStateRef(users: [user], userDb: false, isDeleted: null));
-      await tester.pumpWidget(ProviderScope(child: MyApp()));
-      expect(find.byKey(ValueKey('CircularProfilePage')), findsNothing);
+    testWidgets('Нет данных', (tester) async {
+      await tester.pumpWidget(ProviderScope(child: MyApp(), overrides: [
+        userPodFake.overrideWith((ref) => UserStateRef(users: [
+              UserDto(
+                  name: '',
+                  email: '',
+                  createdDate: '',
+                  profilePicLink: '',
+                  updatedDate: '')
+            ]))
+      ]));
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+      expect(find.byType(Text), findsNothing);
+    });
+
+    testWidgets('Юзер не заполнен', (tester) async {
+      await tester.pumpWidget(ProviderScope(child: MyApp(), overrides: [
+        userPodFake.overrideWith((ref) => UserStateRef(users: [
+              UserDto(
+                  name: '',
+                  email: '',
+                  createdDate: '',
+                  profilePicLink: '',
+                  updatedDate: '')
+            ]))
+      ]));
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+      expect(find.byType(Text), findsNothing);
+    });
+
+    testWidgets('Все ок', (tester) async {
+      await tester.pumpWidget(ProviderScope(child: MyApp(), overrides: [
+        userPodFake.overrideWith((ref) => UserStateRef(users: [
+              UserDto(
+                  name: 'Test',
+                  email: '',
+                  createdDate: '',
+                  profilePicLink: '',
+                  updatedDate: '')
+            ]))
+      ]));
+      expect(find.byType(CircularProgressIndicator), findsNothing);
+      expect(find.byType(Text), findsOneWidget);
     });
   });
 }
