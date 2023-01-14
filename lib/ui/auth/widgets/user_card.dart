f1d518bb8ee8ede7bf3c31d50a39f5ee870b7624
x4dead <muratovargen0@gmail.com>
2023-01-15 01:51:04 +0600

    add custom widget cached_image_widget, add checing password, add confirm_password_dialog

--- lib/ui/auth/widgets/user_card.dart
+++ lib/ui/auth/widgets/user_card.dart
@@ -23,6 +23,10 @@ class UserCard extends StatelessWidget {
                 //await DBHelperStart.instanse.close();
                 var db = await DBHelper.instanse.initDB();
                 print("db open? ${db.path},${db.isOpen}");
+                showDialog(
+                  context: context,
+                  builder: (context) => ConfirmPasswordDialog(user: user),
+                );
 
                 ///TODO: Проверка пароля пользователя
                 ///1. Сверить Пароль введенный с паролем на сервере
@@ -33,46 +37,13 @@ class UserCard extends StatelessWidget {
                 ///1.Проерить Таблицу MainUser на наличие юзера
                 ///2.Проверить поля в таблице User
                 ///Если нет, вывести ошибку "Попробуйте еще раз", удалить базу
-
-                Navigator.of(context).pushNamed(MainLayout.routeName);
               },
               // CachedNetworkImage позволяет кешировать изображение
-              child: CachedNetworkImage(
-                width: 125,
+              child: CachedImageWidget(
+                url: user.profilePicLink,
+                errorText: 'sorry, your image\ncould not be displayed',
                 height: 125,
-                imageUrl: user.profilePicLink,
-                imageBuilder: (context, imageProvider) => ImageCard(
-                  image: DecorationImage(
-                    image: imageProvider,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                // placeholder будет ставить виджет пока загружается картинка
-                placeholder: (context, url) => const ImageCard(
-                  child: Center(child: CircularProgressIndicator()),
-                ),
-                // errorWidget же при возникновении ошибки отображать
-                errorWidget: (context, url, error) => ImageCard(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.error,
-                        color: Theme.of(context).errorColor,
-                        size: 26,
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        'sorry, your image\ncould not be displayed',
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context)
-                            .textTheme
-                            .subtitle2
-                            ?.copyWith(fontSize: 12),
-                      ),
-                    ],
-                  ),
-                ),
+                width: 125,
               ),
             ),
             Text(
---
+++ lib/ui/widgets/custom_widgets/cached_image_widget.dart
@@ -0,0 +1,57 @@
+﻿import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+import '../../../domain/data/dto/user_dto/user_dto.dart';
+import '../../auth/authorization_page.dart';
+
+class CachedImageWidget extends StatelessWidget {
+  const CachedImageWidget({
+    Key? key,
+    required this.url,
+    required this.width,
+    required this.height,
+    this.errorText,
+  }) : super(key: key);
+
+  final String url;
+  final double width;
+  final double height;
+  final String? errorText;
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      width: width,
+      height: height,
+      imageUrl: url,
+      imageBuilder: (context, imageProvider) => ImageCard(
+        image: DecorationImage(
+          image: imageProvider,
+          fit: BoxFit.cover,
+        ),
+      ),
+      // placeholder будет ставить виджет пока загружается картинка
+      placeholder: (context, url) => const ImageCard(
+        child: Center(child: CircularProgressIndicator()),
+      ),
+      // errorWidget же при возникновении ошибки отображать
+      errorWidget: (context, url, error) => ImageCard(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.error,
+              color: Theme.of(context).errorColor,
+              size: 26,
+            ),
+            Text(
+              errorText ?? '',
+              textAlign: TextAlign.center,
+              style:
+                  Theme.of(context).textTheme.subtitle2?.copyWith(fontSize: 12),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
